ca22feb980512c1a5a316b7b71435194fae37ae2
Alexandre Bini <alexandrebini@gmail.com>
2017-07-28 16:32:45 -0300

    style fix

--- spec/factories/story_factory.rb
+++ spec/factories/story_factory.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :story do
     hn_id { generate(:hn_id) }
     published_at { Time.now }
-    sequence :title { |n| "Who is hiring #{ n }" }
+    sequence :title { |n| "Who is hiring #{n}" }
   end
 end

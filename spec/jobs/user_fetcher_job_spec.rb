8d02bd9af04f471c9d14bf9f903eaf7fbc3f3b10
Alexandre Bini <alexandrebini@gmail.com>
2017-07-29 15:13:13 -0300

    Jobs specs

--- spec/jobs/user_fetcher_job_spec.rb
+++ spec/jobs/user_fetcher_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe UserFetcherJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UserFetcherJob, type: :job, vcr: true do
+  subject(:job) { described_class.perform_later }
+  let(:queue_adapter) { ActiveJob::Base.queue_adapter }
+
+  describe '#perform' do
+    it 'enqueue StoryProcessJob for each story_id fetched' do
+      allow(job).to receive(:stories_ids).and_return([1, 2, 3])
+      expect { job.perform }.to change(queue_adapter.enqueued_jobs, :size).by(3)
+    end
+  end
+
+  describe '#stories_ids' do
+    subject { job.send(:stories_ids) }
+
+    context 'success response' do
+      before { allow(job.send(:response)).to receive(:success?).and_return(true) }
+      it { is_expected.to be_an_instance_of(Array) }
+    end
+
+    context 'errored response' do
+      before { allow(job.send(:response)).to receive(:success?).and_return(false) }
+      it { is_expected.to be_an_instance_of(Array) }
+    end
+  end
 end
